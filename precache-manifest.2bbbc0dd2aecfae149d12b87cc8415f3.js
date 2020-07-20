c8fa46985448635118b149d0ca4680572353436b
AttilaPravda <attilabre@gmail.com>
2020-07-20 11:54:59 +0200

    Updates

---
+++ precache-manifest.2bbbc0dd2aecfae149d12b87cc8415f3.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c876a41870b3e97b4825654fddf2cad9",
+    "url": "/ljos-grid/index.html"
+  },
+  {
+    "revision": "766a394f2ca2efdd9de4",
+    "url": "/ljos-grid/static/css/main.36e1476c.chunk.css"
+  },
+  {
+    "revision": "3c091cf0325a144e0f1a",
+    "url": "/ljos-grid/static/js/2.d3e53c40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ljos-grid/static/js/2.d3e53c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766a394f2ca2efdd9de4",
+    "url": "/ljos-grid/static/js/main.6db16298.chunk.js"
+  },
+  {
+    "revision": "20beb5e496986d5db868",
+    "url": "/ljos-grid/static/js/runtime-main.ce4193e1.js"
+  },
+  {
+    "revision": "0c5a70d7863356f761c1fd3de4237dbe",
+    "url": "/ljos-grid/static/media/LexendDecaRegular.0c5a70d7.woff"
+  },
+  {
+    "revision": "9f569b88357838b2366c9088337aa9c4",
+    "url": "/ljos-grid/static/media/game-1-back-big.9f569b88.png"
+  },
+  {
+    "revision": "2cc71c386ce50720df71d4623a34ea8a",
+    "url": "/ljos-grid/static/media/game-1-back-small.2cc71c38.png"
+  },
+  {
+    "revision": "27831907e0084e6b6f590def427bd920",
+    "url": "/ljos-grid/static/media/game-1-big.27831907.png"
+  },
+  {
+    "revision": "843877fdc5fea4861999cd468e94dc03",
+    "url": "/ljos-grid/static/media/game-1-logo.843877fd.png"
+  },
+  {
+    "revision": "6217e3fa12c279f83e63a7b5dd242b95",
+    "url": "/ljos-grid/static/media/game-1-small.6217e3fa.png"
+  },
+  {
+    "revision": "b5fe02270d4f0ed793c329d61ea259ec",
+    "url": "/ljos-grid/static/media/game-2-back-big.b5fe0227.png"
+  },
+  {
+    "revision": "66b1a2c23ca6b515a387820eb021d267",
+    "url": "/ljos-grid/static/media/game-2-back-small.66b1a2c2.png"
+  },
+  {
+    "revision": "9359d50498b9720445a146391122c5f9",
+    "url": "/ljos-grid/static/media/game-2-big.9359d504.png"
+  },
+  {
+    "revision": "0ab4bd292eec446d37d01c8e4b2acb14",
+    "url": "/ljos-grid/static/media/game-2-logo.0ab4bd29.png"
+  },
+  {
+    "revision": "a0d23caa37f065af9a6cb93d7a6662c6",
+    "url": "/ljos-grid/static/media/game-2-small.a0d23caa.png"
+  },
+  {
+    "revision": "52627d333a4e9356b1e7e3d298d3fbc3",
+    "url": "/ljos-grid/static/media/game-3-back-big.52627d33.png"
+  },
+  {
+    "revision": "8ed8b05d5737afe8623c3433a275e30b",
+    "url": "/ljos-grid/static/media/game-3-back-small.8ed8b05d.png"
+  },
+  {
+    "revision": "30a20116df8566425cd5da7059d0f3a5",
+    "url": "/ljos-grid/static/media/game-3-big.30a20116.png"
+  },
+  {
+    "revision": "ded8df2cf4a8e29f17827dcc3c11185f",
+    "url": "/ljos-grid/static/media/game-3-logo.ded8df2c.png"
+  },
+  {
+    "revision": "419b7badb57fbf9a1a80c30725db86ac",
+    "url": "/ljos-grid/static/media/game-3-small.419b7bad.png"
+  },
+  {
+    "revision": "3a3b9d0e027e7e957a6af189153291bf",
+    "url": "/ljos-grid/static/media/game-4-back-big.3a3b9d0e.png"
+  },
+  {
+    "revision": "8582d22dc4f3bf1e57f5ef068b893613",
+    "url": "/ljos-grid/static/media/game-4-back-small.8582d22d.png"
+  },
+  {
+    "revision": "d71393eec002ba877f33be1a047757c6",
+    "url": "/ljos-grid/static/media/game-4-big.d71393ee.png"
+  },
+  {
+    "revision": "7991745945ec565d87b011531a800b71",
+    "url": "/ljos-grid/static/media/game-4-logo.79917459.png"
+  },
+  {
+    "revision": "fa5aa1c47da52e09b15269643321fca4",
+    "url": "/ljos-grid/static/media/game-4-small.fa5aa1c4.png"
+  },
+  {
+    "revision": "7228e992774f776700ed9d66eb6236de",
+    "url": "/ljos-grid/static/media/game-5-back-big.7228e992.png"
+  },
+  {
+    "revision": "30beaaac5088ec8ac9068ddcb2ad5192",
+    "url": "/ljos-grid/static/media/game-5-back-small.30beaaac.png"
+  },
+  {
+    "revision": "adbaa0cce6eddff65b671b3171e8fe3e",
+    "url": "/ljos-grid/static/media/game-5-big.adbaa0cc.png"
+  },
+  {
+    "revision": "d2ccf3f7af46a2819eeaf34a5b2ed968",
+    "url": "/ljos-grid/static/media/game-5-logo.d2ccf3f7.png"
+  },
+  {
+    "revision": "4329b89ffff40d94f629328d27c452fa",
+    "url": "/ljos-grid/static/media/game-5-small.4329b89f.png"
+  },
+  {
+    "revision": "c2ea629e8d2f6232d4cedaa16802489e",
+    "url": "/ljos-grid/static/media/game-6-back-big.c2ea629e.png"
+  },
+  {
+    "revision": "6dfe3102150e7c8b0f84af389cc82cbf",
+    "url": "/ljos-grid/static/media/game-6-back-small.6dfe3102.png"
+  },
+  {
+    "revision": "7ff849d5afee2282890a5aca37cceadb",
+    "url": "/ljos-grid/static/media/game-6-big.7ff849d5.png"
+  },
+  {
+    "revision": "6f5c26d73eba73a7d5c8cd98e505ca29",
+    "url": "/ljos-grid/static/media/game-6-logo.6f5c26d7.png"
+  },
+  {
+    "revision": "ceb383145b676b8acdf98940992d8168",
+    "url": "/ljos-grid/static/media/game-6-small.ceb38314.png"
+  },
+  {
+    "revision": "a6864de7b9e24ceee6a45a1b0a2540d3",
+    "url": "/ljos-grid/static/media/scott-webb-tMuYJL96A_I-unsplash.a6864de7.png"
+  },
+  {
+    "revision": "32a1b359c9a8d90549a14e2ad99654ee",
+    "url": "/ljos-grid/static/media/scott-webb-tMuYJL96A_I-unsplash@2x.32a1b359.png"
+  }
+]);
